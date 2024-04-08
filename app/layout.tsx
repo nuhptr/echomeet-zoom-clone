62e77a3f96e1b9d3479697a1dc2ac6deedbf5837
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2024-04-08 12:38:53 +0700

    app:layout add clerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
 
 import "./globals.css"
 
@@ -17,9 +18,26 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} bg-dark-2`} suppressHydrationWarning={true}>
-                {children}
-            </body>
+            <ClerkProvider
+                appearance={{
+                    layout: {
+                        logoImageUrl: "/icons/ic_yoom_logo.svg",
+                        socialButtonsVariant: "iconButton",
+                    },
+                    variables: {
+                        colorText: "#fff",
+                        colorPrimary: "#0E78F9",
+                        colorBackground: "#1c1f2e",
+                        colorInputBackground: "#252a41",
+                        colorInputText: "#fff",
+                    },
+                }}>
+                <body
+                    className={`${inter.className} bg-dark-2 no-scrollbar`}
+                    suppressHydrationWarning={true}>
+                    {children}
+                </body>
+            </ClerkProvider>
         </html>
     )
 }
