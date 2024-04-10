f069c26699356647b8c835e6114c26089728326e
nuhptr <nugrahaadi733@gmail.com>
2024-04-10 19:37:07 +0700

    app/(root):layout update code

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,20 @@
+import { Metadata } from "next"
+
+import { StreamVideoProvider } from "@/providers/StreamClientProviders"
+
+export const metadata: Metadata = {
+    title: "Echomeet | Next generation video conferencing",
+    description:
+        "Echomeet is a next generation video conferencing platform built from modern tech.",
+    icons: {
+        icon: "/icons/logo.svg",
+    },
+}
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return <main>{children}</main>
+    return (
+        <main>
+            <StreamVideoProvider>{children}</StreamVideoProvider>
+        </main>
+    )
 }
