5feda12bd19516d19c05bc477470b31eed5d2bdc
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 17:58:38 +0700

    components: loader formatting code

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
 
-export default function Loader() {
-    return (
-        <div className="w-full h-screen flex-center">
-            <Image src="/icons/ic_loading_circle.svg" alt="Loading Icon" width={50} height={50} />
-        </div>
-    )
+const Loader = () => {
+   return (
+      <div className="w-full h-screen flex-center">
+         <Image src="/icons/ic_loading_circle.svg" alt="Loading Icon" width={50} height={50} />
+      </div>
+   )
 }
+
+export default Loader
