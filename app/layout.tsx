a56ad134112b74e28bac52d10219861659ee0690
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 16:52:05 +0700

    app: layout add comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
    return (
       <html lang="en">
+         {/* Clerk Provider in HERE */}
          <ClerkProvider
             appearance={{
                layout: {
