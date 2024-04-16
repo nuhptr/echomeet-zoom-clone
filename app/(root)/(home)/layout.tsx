fff7b97e2df6744e0e39d3fd5771edc65f97bf4b
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 17:49:53 +0700

    root/home: layout updated code

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,28 +1,31 @@
+import { ReactNode } from "react"
 import { Metadata } from "next"
 
 import Navbar from "@/components/Navbar"
 import Sidebar from "@/components/Sidebar"
 
 export const metadata: Metadata = {
-    title: "Echomeet | Next generation video conferencing",
-    description:
-        "Echomeet is a next generation video conferencing platform built from modern tech.",
-    icons: {
-        icon: "/icons/logo.svg",
-    },
+   title: "Echomeet | Next generation video conferencing",
+   description: "Echomeet is a video conferencing platform built from modern tech.",
+   icons: {
+      icon: "/icons/logo.svg",
+   },
 }
 
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <main className="relative">
-            <Navbar />
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+   return (
+      <main className="relative">
+         <Navbar />
 
-            <div className="flex">
-                <Sidebar />
-                <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-                    <div className="w-full">{children}</div>
-                </section>
-            </div>
-        </main>
-    )
+         <div className="flex">
+            <Sidebar />
+
+            <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+               <div className="w-full">{children}</div>
+            </section>
+         </div>
+      </main>
+   )
 }
+
+export default HomeLayout
