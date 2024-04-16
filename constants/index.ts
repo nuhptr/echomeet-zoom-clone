755aa5fa285073f3fbc4d1389d7d7f70b9939403
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 17:02:35 +0700

    constant: formatting code

--- constants/index.ts
+++ constants/index.ts
@@ -1,35 +1,15 @@
 export const sidebarLinks = [
-    {
-        imgURL: "/icons/ic_home.svg",
-        route: "/",
-        label: "Home",
-    },
-    {
-        imgURL: "/icons/ic_upcoming.svg",
-        route: "/upcoming",
-        label: "Upcoming",
-    },
-    {
-        imgURL: "/icons/ic_previous.svg",
-        route: "/previous",
-        label: "Previous",
-    },
-    {
-        imgURL: "/icons/ic_video.svg",
-        route: "/recordings",
-        label: "Recordings",
-    },
-    {
-        imgURL: "/icons/ic_add_personal.svg",
-        route: "/personal-room",
-        label: "Personal Room",
-    },
+   { imgURL: "/icons/ic_home.svg", route: "/", label: "Home" },
+   { imgURL: "/icons/ic_upcoming.svg", route: "/upcoming", label: "Upcoming" },
+   { imgURL: "/icons/ic_previous.svg", route: "/previous", label: "Previous" },
+   { imgURL: "/icons/ic_video.svg", route: "/recordings", label: "Recordings" },
+   { imgURL: "/icons/ic_add_personal.svg", route: "/personal-room", label: "Personal Room" },
 ]
 
 export const avatarImages = [
-    "/images/avatar-1.jpeg",
-    "/images/avatar-2.jpeg",
-    "/images/avatar-3.png",
-    "/images/avatar-4.png",
-    "/images/avatar-5.png",
+   "/images/avatar-1.jpeg",
+   "/images/avatar-2.jpeg",
+   "/images/avatar-3.png",
+   "/images/avatar-4.png",
+   "/images/avatar-5.png",
 ]
