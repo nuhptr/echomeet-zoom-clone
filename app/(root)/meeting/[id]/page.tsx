a5d2e605332c67b50335bdb66cf99ba5d961ca55
nuhptr <nugrahaadi733@gmail.com>
2024-04-16 17:50:35 +0700

    root/meeting: updated code

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,34 +1,47 @@
 "use client"
 
 import { useState } from "react"
+import { useParams } from "next/navigation"
 import { useUser } from "@clerk/nextjs"
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk"
+import { Loader } from "lucide-react"
 
+import Alert from "@/components/Alert"
 import MeetingSetup from "@/components/MeetingSetup"
 import MeetingRoom from "@/components/MeetingRoom"
-import Loader from "@/components/Loader"
 
 import { useGetCallById } from "@/hooks/useGetCallById"
 
-export default function Meeting({ params: { id } }: { params: { id: string } }) {
-    const { user, isLoaded } = useUser()
-    const [isSetupComplete, setIsSetupComplete] = useState(false)
+const MeetingPage = () => {
+   const { id } = useParams()
+   const { isLoaded, user } = useUser()
 
-    const { call, isCallLoading } = useGetCallById(id)
+   const { call, isCallLoading } = useGetCallById(id)
+   const [isSetupComplete, setIsSetupComplete] = useState(false)
 
-    if (!isLoaded || isCallLoading) return <Loader />
+   if (!isLoaded || isCallLoading) return <Loader />
 
-    return (
-        <main className="w-full h-screen">
-            <StreamCall call={call}>
-                <StreamTheme>
-                    {!isSetupComplete ? (
-                        <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-                    ) : (
-                        <MeetingRoom />
-                    )}
-                </StreamTheme>
-            </StreamCall>
-        </main>
-    )
+   if (!call) return <p className="text-3xl font-bold text-center text-white">Call Not Found</p>
+
+   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+   const notAllowed =
+      call.type === "invited" && (!user || !call.state.members.find((m) => m.user.id === user.id))
+
+   if (notAllowed) return <Alert title="You are not allowed to join this call" />
+
+   return (
+      <main className="w-full h-screen">
+         <StreamCall call={call}>
+            <StreamTheme>
+               {!isSetupComplete ? (
+                  <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+               ) : (
+                  <MeetingRoom />
+               )}
+            </StreamTheme>
+         </StreamCall>
+      </main>
+   )
 }
+
+export default MeetingPage
